75dcd819f0382dec69c61e37aa8c2fee5ef3daf6
jeanmfdias <jeanmfdias@gmail.com>
2021-08-19 21:16:18 -0300

    add post route

--- controllers/calls.js
+++ controllers/calls.js
@@ -1,5 +1,9 @@
 module.exports = app => {
     app.get('/calls',  (req, res) => {
         res.send('you are calls route');
-    });    
+    });
+    
+    app.post('/calls', (req, res) => {
+        res.send('you are calls route with post');
+    });
 }
