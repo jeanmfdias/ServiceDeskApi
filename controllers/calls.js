ed704683f8f20eb82812f340dbb035bb3e47903a
jeanmfdias <jeanmfdias@gmail.com>
2022-07-04 21:44:13 -0300

    remove node express project sample

--- controllers/calls.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-    app.get('/calls',  (req, res) => {
-        res.send('you are calls route');
-    });
-    
-    app.post('/calls', (req, res) => {
-        res.send('you are calls route with post');
-    });
-}
--- index.js
+++
@@ -1,5 +0,0 @@
-const customExpress = require('./config/custom_express');
-
-const app = customExpress();
-
-app.listen(3000, () => console.log('server runnig at 3000 port'));
