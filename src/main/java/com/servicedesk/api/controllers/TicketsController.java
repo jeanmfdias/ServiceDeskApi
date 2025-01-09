61b0afb75af5e48b54cba73b3e2a1f4fcb9fc042
jeanmfdias <jeanmfdias@gmail.com>
2025-01-09 18:45:29 -0300

    Add tickets resources

---
+++ src/main/java/com/servicedesk/api/controllers/TicketsController.java
@@ -0,0 +1,25 @@
+package com.servicedesk.api.controllers;
+
+import com.servicedesk.api.entities.Ticket;
+import com.servicedesk.api.repositories.TicketRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tickets")
+public class TicketsController {
+    private TicketRepository ticketRepository;
+
+    public TicketsController(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+    public ResponseEntity<List<Ticket>> getAll() {
+        List<Ticket> tickets = this.ticketRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(tickets);
+    }
+}
