a86d4ab3c6ece2c6845fa61f6794ec7245705225
jeanmfdias <jeanmfdias@gmail.com>
2025-02-18 08:01:38 -0300

    Add spring validation

--- src/main/java/com/servicedesk/api/entities/dto/CreateUserDto.java
+++ src/main/java/com/servicedesk/api/entities/dto/CreateUserDto.java
@@ -1,4 +1,15 @@
 package com.servicedesk.api.entities.dto;
 
-public record CreateUserDto(String name, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateUserDto(@NotBlank
+                            String name,
+
+                            @NotBlank
+                            @Email
+                            String email,
+
+                            @NotBlank
+                            String password) {
 }
